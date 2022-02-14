bd968e022bd40916d3d5beec04ab2e34f2b6f64c
Mamadou Diallo <mmsd228@cornell.edu>
2022-02-14 21:26:51 +0000

    created initial components for the app

---
+++ src/app/job-card/job-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+import { InfoComponent } from '../info/info.component';
+@Component({
+  selector: 'app-job-card',
+  templateUrl: './job-card.component.html',
+  styleUrls: ['./job-card.component.css']
+})
+export class JobCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
