2a8f6831a4ce94773b2da7f02d5ae44b75c7f228
Mamadou Diallo <mamadousaliou@google.com>
2022-07-31 16:23:34 +0000

    Made changes to the front end for signup and login forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import { MatSliderModule } from '@angular/material/slider';
+
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,8 +21,12 @@ import { JobDescriptionComponent } from './job-description/job-description.compo
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { CreateJobComponent } from './create-job/create-job.component';
+import { UpdateJobComponent } from './update-job/update-job.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // import { JobSkillsService } from './services/job-skills/job-skills.service';
 // import { JobSkillsModel } from './models/job-skills.model';
+
 
 @NgModule({
   declarations: [
@@ -31,15 +41,25 @@ import { SignupComponent } from './signup/signup.component';
     HomePageComponent,
     LoginComponent,
     SignupComponent,
+    CreateJobComponent,
+    UpdateJobComponent,
     // JobSkillsService,
     // JobSkillsModel
+    
   ],
   imports: [
     FormsModule,
+    ReactiveFormsModule,
+    MatTooltipModule,
     HttpClientModule,
-    
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatSliderModule,
+
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/update-job/update-job.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-job',
+  templateUrl: './update-job.component.html',
+  styleUrls: ['./update-job.component.css']
+})
+export class UpdateJobComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
