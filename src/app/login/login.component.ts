56f833db941c6b3e3835b097d988d4033e3d0dcd
Mamadou Diallo <mamadousaliou@google.com>
2022-07-31 23:24:44 +0000

    added some code for login and sign up

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
-// import { Jobs } from '../filter/filter.component';
+import {Router} from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { UsersModel } from '../models/users.model';
+import  { UsersService } from '../services/users.service'
 
 @Component({
   selector: 'app-login',
@@ -8,9 +11,48 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   
-  constructor() { }
+  @Input() users?: UsersModel[];
 
+  logInForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  })
+
+  constructor(private usersService: UsersService, private route:Router ) {}
+
+  onSubmit() {
+    const user = this.logInForm.value;
+    console.log(this.users)
+    if (this.users) {
+      for(let i = 0; i < this.users.length; i++) {
+        if(this.users[i].email == user.email) {
+          console.log(user.email, this.users[i].email, "exist");
+          if (this.users[i].password == user.password) {
+            this.route.navigate(['/home']);
+            break;
+          } else {
+            console.log("Wrong password");
+            break;
+          }
+        }
+        console.log("This account doesn't exist. Pelase sign up");
+          this.route.navigate(['/signup']);
+      }
+    }
+  }
+
+
+  retrieveUsers(): void {
+    this.usersService.getAll()
+      .subscribe({
+        next: (data) => {
+          this.users = data;
+        },
+        error: (e) => console.error(e)
+      });
+  }
   ngOnInit(): void {
+    this.retrieveUsers();
   }
 
 }
