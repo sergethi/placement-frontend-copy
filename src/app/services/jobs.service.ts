6fc8df539985c241c4cce5f1ae08e32740d99e2e
Mamadou Diallo <mamadousaliou@google.com>
2022-08-02 22:41:10 +0000

    frontend

--- src/app/services/jobs.service.ts
+++ src/app/services/jobs.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { JobsModel } from '../models/jobs.model';
-const baseUrl = 'http://localhost:3005/jobs';
+const getJobs = 'http://localhost:3005/jobs';
 @Injectable({
   providedIn: 'root'
 })
@@ -10,6 +10,6 @@ const baseUrl = 'http://localhost:3005/jobs';
 export class JobsService {
     constructor(private http: HttpClient) { }
   getAll(): Observable<JobsModel[]> {
-    return this.http.get<JobsModel[]>(baseUrl);
+    return this.http.get<JobsModel[]>(getJobs);
   }
 }
