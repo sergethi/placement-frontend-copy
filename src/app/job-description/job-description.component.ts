b8c27ae86cb199d89d6c81c4ff546c40ef32d970
Mamadou Diallo <mamadousaliou@google.com>
2022-08-03 20:01:34 +0000

    Job description is 80% complete. need css and bug fixing

--- src/app/job-description/job-description.component.ts
+++ src/app/job-description/job-description.component.ts
@@ -1,18 +1,49 @@
-import { Component, OnInit, Input} from '@angular/core';
-import data from './../_files/data.json';
+import { Component, OnInit, AfterViewInit, Input} from '@angular/core';
+import {Router} from '@angular/router';
+import { JobsModel } from '../models/jobs.model';
+import { UsersService } from '../services/users.service';
+import { JobsService } from '../services/jobs.service';
 
 @Component({
   selector: 'app-job-description',
   templateUrl: './job-description.component.html',
   styleUrls: ['./job-description.component.css']
 })
-export class JobDescriptionComponent implements OnInit {
-  @Input() jobDetail: any = data;
-  @Input() jobId: number = 1;
-  @Input() job: object = this.jobDetail[this.jobId]
+export class JobDescriptionComponent implements AfterViewInit, OnInit {
+  jobs: JobsModel[];
+  job: JobsModel;
+  jobId: number;
 
-  constructor() { }
+  constructor(private route:Router, private usersService: UsersService, private jobsService: JobsService) {}
+
+
+  retrieveUsers(): void {
+    this.jobsService.getAll().subscribe({
+      next: (data) => {
+        for(let i = 0; i <data.length; i++) {
+          if(data[i].id == this.jobId) {
+            this.job = data[i];
+            break;
+          }
+        }
+      },
+      error: (e) => console.error(e),
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.retrieveUsers();
+  }
 
   ngOnInit(): void {
+    this.jobId = this.jobsService.currentJobId;
+    this.jobsService.getCurrentJob(this.jobId).subscribe({
+      next: (data) => {
+        this.job = data;
+      }, 
+      error: (e) => {
+        console.log(e)
+      }
+    })
   }
 }
